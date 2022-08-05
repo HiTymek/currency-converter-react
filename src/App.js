43d4f303d78e10bcb71637fb36650e0df14b1724
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-08-05 20:51:07 +0200

    delete unused attributes and add general styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
 function App() {
   return (
-    <div></div>
+    <div className="container">
+      <header>
+        <h1 className="header">WIRTUALNY KANTOR CENTUŚ 😎</h1>
+      </header>
+
+      <main className="main">
+
+        <form className="form">
+
+          <fieldset className="form__fieldset">
+
+            <legend className="form__legend">WYMIANA WALUT</legend>
+
+            <p className="form__paragraph">
+              <label className="form__label">WYBIERZ WALUTE</label>
+              <select className="form__field">
+                <option>EUR</option>
+                <option>USD</option>
+                <option>GBP</option>
+                <option>CHF</option>
+                <option>UAH</option>
+              </select>
+            </p>
+
+            <p className="form__paragraph">
+              <label className="form__label">WPISZ KWOTĘ W PLN</label>
+              <input className="form__field" type="number" id="amount" min="1" step="0.1"
+                placeholder="Kwotę podaj w PLN" />
+            </p>
+
+            <p className="form__paragraph">WYNIK : <strong></strong></p>
+
+            <p className="form__paragraph">
+              <button className="form__button">PRZELICZ</button>
+            </p>
+
+          </fieldset>
+
+        </form>
+      </main>
+    </div>
   );
 }
 
