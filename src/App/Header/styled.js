1a6d966ef5bd261df6251d4251553a54851c46cf
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-10-01 13:04:20 +0200

    styled-components added to Header component

--- src/App/Header/styled.js
+++ src/App/Header/styled.js
@@ -1,7 +1,9 @@
-.header {
+import styled from "styled-components";
+
+export const StyledHeader = styled.h1`
     text-align: center;
     color: teal;
     padding: 35px 0;
     margin: 0;
     font-size: 26px;
-}
+`;
