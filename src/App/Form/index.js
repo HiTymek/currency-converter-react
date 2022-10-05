3daf8f08f79d12b1400d69d4e692303536128432
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-10-05 22:49:19 +0200

    now app takes currencies and rates from api / no from array

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,48 +1,15 @@
 import InputContainer from "./InputContainer";
 import CurrencySelect from "./CurrencySelect";
 import Clock from "./Clock";
-import { currencies } from "../currencies";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { StyledForm, Fieldset, Legend, Input, Button } from "./styled";
-import axios from "axios";
-
-const useRatesData = () => {
-    const [ratesData, setRatesData] = useState({
-        state: "pending",
-    });
-
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await axios.get("https://api.exchangerate.host/latest");
-                const date = response.data.date;
-                const rates = response.data.rates;
-                setRatesData({
-                    state: "succes",
-                    date,
-                    rates,
-                });
-            } catch (error) {
-                setRatesData({
-                    state: "error",
-                });
-                console.log("Something went wrong", error);
-            }
-        };
-
-        setTimeout(getData, 1000);
-
-    }, []);
-
-    return ratesData;
-};
-
+import { useRatesData } from "./useRatesData";
 
 const Form = ({ legend }) => {
 
     const ratesData = useRatesData();
 
-    const [currency, setCurrency] = useState(currencies[0].name);
+    const [currency, setCurrency] = useState("");
     const [amount, setAmount] = useState("");
     const [result, setResult] = useState(null);
 
@@ -52,56 +19,62 @@ const Form = ({ legend }) => {
     };
 
     const calculateResult = () => {
-        const rate = currencies.find(({ name }) => name === currency).exchangeRate;
+        console.log(ratesData.rates[currency])
+        const rate = ratesData.rates[currency];
         setResult({
             amount,
             finalResult: (+amount / rate).toFixed(2),
-            currency: currencies.find(({ name }) => name === currency).code,
+            currency: currency,
         });
     };
 
     return (
-        <StyledForm
-            onSubmit={onFormSubmit}
-        >
+        <>
+            {ratesData.state === "pending" ? <div></div>
+                :
+                <StyledForm
+                    onSubmit={onFormSubmit}
+                >
+                    <Fieldset>
+                        <Legend>{legend}</Legend>
 
-            <Fieldset>
-                <Legend>{legend}</Legend>
+                        <Clock />
 
-                <Clock />
+                        <InputContainer label="WYBIERZ WALUTE">
+                            <CurrencySelect
+                                ratesData={ratesData}
+                                currency={currency}
+                                setCurrency={setCurrency}
+                            />
+                        </InputContainer>
 
-                <InputContainer label="WYBIERZ WALUTE">
-                    <CurrencySelect
-                        currencies={currencies}
-                        currency={currency}
-                        setCurrency={setCurrency}
-                    />
-                </InputContainer>
+                        <InputContainer label="WPISZ KWOTĘ W PLN">
+                            <Input
+                                type="number"
+                                min="1"
+                                step="0.01"
+                                placeholder="Kwotę podaj w PLN"
+                                value={amount}
+                                onChange={({ target }) => setAmount(target.value)}
+                            />
+                        </InputContainer>
 
-                <InputContainer label="WPISZ KWOTĘ W PLN">
-                    <Input
-                        type="number"
-                        min="1"
-                        step="0.01"
-                        placeholder="Kwotę podaj w PLN"
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                    />
-                </InputContainer>
+                        <InputContainer label="WYNIK">
+                            <strong>
+                                {result && `${result.amount} PLN = ${result.finalResult} ${result.currency}`}
+                            </strong>
+                        </InputContainer>
 
-                <InputContainer label="WYNIK">
-                    <strong>
-                        {result && `${result.amount} PLN = ${result.finalResult} ${result.currency}`}
-                    </strong>
-                </InputContainer>
+                        <InputContainer label="PRZELICZ ILE DOSTANIESZ">
+                            <Button>PRZELICZ</Button>
+                        </InputContainer>
 
-                <InputContainer label="PRZELICZ ILE DOSTANIESZ">
-                    <Button>PRZELICZ</Button>
-                </InputContainer>
+                    </Fieldset>
 
-            </Fieldset>
+                </StyledForm>
+            }
 
-        </StyledForm>
+        </>
     );
 };
 
